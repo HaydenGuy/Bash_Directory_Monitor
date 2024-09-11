852dc1637aafb3aad2f533462d874d5f8448410c
hayden <haydenguyn@gmail.com>
2024-09-11 14:59:35 -0700

    extra comment #

--- directory_monitor.sh
+++ directory_monitor.sh
@@ -18,15 +18,15 @@ output_video_dir="${output_root_dir}/video"
 output_image_dir="${output_root_dir}/image"
 output_audio_dir="${output_root_dir}/audio"
 
-# # Monitor the input directory for new file creation events
-# # -m: Monitor mode, keeps inotifywait running continuously
-# # -e create: Only triggers on file creation events in the monitored directory
+# Monitor the input directory for new file creation events
+# -m: Monitor mode, keeps inotifywait running continuously
+# -e create: Only triggers on file creation events in the monitored directory
 inotifywait -m "$input_dir" -e create |
 while read path action file; do
-# #     # 'read' command processes output from inotifywait
-# #     #   - 'path': the directory where the event occurred
-# #     #   - 'action': the type of event (in this case, "CREATE")
-# #     #   - 'file': the name of the newly created file
+    # 'read' command processes output from inotifywait
+    #   - 'path': the directory where the event occurred
+    #   - 'action': the type of event (in this case, "CREATE")
+    #   - 'file': the name of the newly created file
     echo "New file detected: $file in directory $path"
     
     mv "$path/$file" "$output_dir/"
